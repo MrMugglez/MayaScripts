ee21df4081451bd3024d070fbccfa14d7234498a
Derek Brouwer <derek.brouwer@yahoo.ca>
2019-01-03 15:47:03 -0800

    updates to dev branch

--- JointOrient/JointOrient.py
+++ JointOrient/JointOrient.py
@@ -5,10 +5,12 @@ channel_x = True
 channel_y = False
 channel_z = False
 
-window = mc.window()
-mc.columnLayout()
+window = mc.window(title="Joint Orientation Tool", iconName='Short Name', widthHeight=(200, 105))
+mc.columnLayout(adjustableColumn=True)
 mc.button(label="Reorient Single Joint", command="reorient_selected()")
 mc.button(label="Reorient Joints", command="reorient_selected()")
+can_reset_orientation = mc.checkBox("Reset Orientation")
+can_reset_rotation = mc.checkBox("Reset Rotation")
 mc.showWindow()
 
 
@@ -21,22 +23,42 @@ def reorient(selection, channel, additive=True):
     mc.setAttr(selection + ".rotate" + channel, 0)
 
 
+def reset_orientation(selection, channel):
+    mc.setAttr(selection + ".jointOrient" + channel, 0)
+
+
+def reset_rotation(selection, channel):
+    mc.setAttr(selection + ".rotate" + channel, 0)
+
+
+def reorient_specific_channel(sel):
+    if channel_x:
+        if not reset_orientation:
+            reorient(sel, "X")
+        else:
+            reset_orientation(sel, "X")
+    if channel_y:
+        if not reset_orientation:
+            reorient(sel, "Y")
+        else:
+            reset_orientation(sel, "Y")
+    if channel_z:
+        if not reset_orientation:
+            reorient(sel, "Z")
+        else:
+            reset_orientation(sel, "Z")
+
+
 def reorient_selected():
     for sel in mc.ls(selection=True):
         if all_channels:
             for channel in ["X", "Y", "Z"]:
-                reorient(sel, channel)
+                if reset_orientation:
+                    reset_orientation(sel, channel)
+                else:
+                    reorient(sel, channel)
         else:
-            if channel_x:
-                reorient(sel, "X")
-            if channel_y:
-                reorient(sel, "Y")
-            if channel_z:
-                reorient(sel, "Z")
-
-
-def revert_orientation():
-    pass
+            reorient_specific_channel(sel)
 
 
 def reorient_tree():
